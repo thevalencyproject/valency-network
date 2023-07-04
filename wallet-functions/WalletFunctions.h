8fc10188f50216422b308eeea2410f7797d1f17c
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-04 23:40:39 +1000

    Added Wallet Functions Header
    Added the header file for the wallet functions framework - this can interface with any wallets (third party included) for the Valency Network. This framework serves to make the network more modular

---
+++ wallet-functions/WalletFunctions.h
@@ -0,0 +1,42 @@
+#ifndef WALLETFUNCTIONS_H
+#define WALLETFUNCTIONS_H
+
+#include <string>
+#include <vector>
+#include "valency-core/networking/onion-routing/Onion.h"
+#include "valency-core/file-recognition/file-writer/FileWriter.h"
+#include "valency-core/file-recognition/file-reader/FileReader.h"
+#include "valency-core/cryptography/ntru-encryption/NTRUencrypt.h"
+#include "valency-core/cryptography/aes-encryption/AES-Encryption.h"
+#include "valency-core/cryptography/traceable-ring-signatures/Ring-Signature.h"
+
+
+class WalletFunctions {
+private:
+    std::string blockchainFilePath = "blockchain.vlnc";     // The blockchain file path
+    std::vector<Shard> blockchain;                          // The Valency Network Blockchain!
+
+public:
+    WalletFunctions() {};
+
+    // Generator Functions
+    std::string generatePublicKey(std::string privateKey);                                                                          // Generates the NTRUencrypt public key from the corresponding private key
+    std::pair<std::string, std::string> generateKeyPair(std::string username, std::string password);                                // Generates an NTRUencyrpt key pair from a username + password (used as seeds)
+    std::pair<std::string, std::string> generateStealthKeyPair(std::string publicKey, std::string privateKey, unsigned int index);  // Index is the number of stealth key pairs already generated + 1
+
+    // Transaction Functions
+    std::pair<std::string, double> requestTransactionFee(bool singleTransaction, int numOfTransactions, std::string receiver, double amount, bool onionRouting, int numOfOnionNodes);   // Returns the transaction fee and expiration time (min 30 seconds)
+    std::pair<bool, TransactionInfo> sendTransaction(bool singleTransaction, int numOfTransactions, std::string receiver, double amount, bool onionRouting, int numOfOnionNodes);       // Sends the transaction
+    
+    // Getter Functions
+    Block getBlock(unsigned int shard, unsigned int block);                // Get a specific block from the blockchain
+    std::vector<Block> getBlock(unsigned int shard, unsigned int block);   // Used to download the blockchain/parts of the blockchain
+
+    // Scanner Functions
+    std::vector<Block> relatedBlocks(std::string privateKey);                                                 // Scans the blockchain and returns a vector of any blocks that are related to the privateKey inputted (sender, receiver, or used as decoy in TRS!)
+    std::vector<TransactionInfo> relatedTransactions(std::string privateKey);                                 // Scans the blockchain and returns a vector of any transactions that are related to the privateKey inputted
+    double walletBalanceCheck(std::string privateKey);                                                        // Checks the wallet balance from the blockchain
+    double walletBalanceCheck(std::string privateKey, std::vector<TransactionInfo> transactionRepository);    // Checks the wallet balance from the inputted transaction repository
+};
+
+#endif
