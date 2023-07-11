b5498379b27d87b05bc6670f9d406c4d41507bd1
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-11 12:41:48 +1000

    Updated Blockchain Sync Header
    Updated the blockchain syncing header, making it easier for functions to interface with each other + adding some key features that were missed

--- sync/blockchain/SyncBlockchain.h
+++ sync/blockchain/SyncBlockchain.h
@@ -12,19 +12,23 @@
 
 struct SyncBlockchain {
 private:
-    bool onion = false;     // Does not use onion routing by default
+    Blockchain* chain;
+    int nodesRequiredForVerification;    // The number of nodes required for a block to be added: 50% of all nodes + 1 node
 
     Server server;
     Client client;
     Onion onion(2);
+    SaveBlockchain save;
+
+    std::string communicate(std::string input);     // Passed into the networking frameworks (client, server, and onion can use same function)
 
 public:
     SyncBlockchain() {};
-    SyncBlockchain(bool onionRouting) { onion = onionRouting };   // true if communicate using the onion routing framework
 
     // Runs in its own core - constantly syncs the blockchain: Ensure the activeNodes vector is also being synced by SyncActiveNodes
     //   -> Designed to take pointers to constantly changing vectors (changed in other threads)
-    void sync(Blockchain* blockchain, std::vector<Position3D>* activeNodes);
+    void sync(Blockchain* blockchain, std::vector<Position3D>* activeNodes);                                // Non Onion-Routing
+    void sync(Blockchain* blockchain, std::vector<Position3D>* activeNodes, std::vector<NodeInfo> nodes);   // Onion-Routing - ensure nodes vector only contains onion-routing nodes and not destination server
 
     void read(Blockchain* blockchain, std::string filePath);    // Also has a file reading/writing functionality through the local-save framework (in valency-network)
     void save(Blockchain* blockchain, std::string filePath);    //   -> filePath should contain the file name / SaveBlockchain save;
