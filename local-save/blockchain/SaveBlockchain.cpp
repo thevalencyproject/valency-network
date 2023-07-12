c7070641e058abe93a11465db80ba87c065b0715
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-12 18:07:17 +1000

    Fixed No Ring Signature Issue
    Fixed the no ring signature issue + some other mistakes with the class.
    Also simplified the logic by removing the millions of counters for a single index integer, incremented each time data is used

--- local-save/blockchain/SaveBlockchain.cpp
+++ local-save/blockchain/SaveBlockchain.cpp
@@ -9,103 +9,162 @@ void SaveBlockchain::read(std::string filepath) {
     path = filepath;
     std::vector<std::string> data = reader.getData(&path);
 
-    blockchain.id = data[0];                   // The blockchain unique identifier
-    blockchain.length = stoul(data[1]);        // The shard length
-    blockchain.numOfShards = stoul(data[2]);   // The number of shards in the chain
+    int blocksRemainingInShard = data[0];      // The number of blocks in the unfilled shard
+    blockchain.id = data[1];                   // The blockchain unique identifier
+    blockchain.length = stoul(data[2]);        // The shard length
+    blockchain.numOfShards = stoul(data[3]);   // The number of shards in the chain
 
-    // numOfShards - 1 to negate the last shard (last shard may not be full)
+    int index = 4;     // The index counter
+
+    // Filled Shards
     for(int i = 1; i <= (blockchain.numOfShards - 1); i++) {
-        unsigned int index = (blockchain.length * 3 * i) + (2 * i) + 3;
+        blockchain.chain[i].shardHash = data[index];
+        blockchain.chain[i].prevShardHash = data[++index];
 
-        blockchain.chain[i].shardHash = data[index + 1];
-        blockchain.chain[i].prevShardHash = data[index + 2];
-
+        // Block
         for(int j = 0; j < blockchain.length; j++) {
-            int blockIndex = (index + 2) + j * 3;   // start at index + 2 -> (index + 2) + (j * 3) + 1, 2, or 3
-            blockchain.chain[i].shard[j].blockHash = data[blockIndex + 1];
-            blockchain.chain[i].shard[j].prevBlockHash = data[blockIndex + 2];
-            blockchain.chain[i].shard[j].time = data[blockIndex + 3];
+            blockchain.chain[i].shard[j].ringsignature.keyImage = data[++index];
+            blockchain.chain[i].shard[j].ringsignature.signature = data[++index];
+
+            int blockIndex = ++index;
+            for(int k = 0; k < data[blockIndex]; k++)   // Ring of public keys here
+                blockchain.chain[i].shard[j].ringsignature.publicKeys.push_back(data[++index]);
+
+            blockchain.chain[i].shard[j].ringsignature.data.receiverKey = data[++index];
+            blockchain.chain[i].shard[j].ringsignature.data.transactionAmount = stoul(data[++index]);
+
+            blockchain.chain[i].shard[j].blockHash = data[++index];
+            blockchain.chain[i].shard[j].prevBlockHash = data[++index];
+            blockchain.chain[i].shard[j].time = data[++index];
         }
     }
 
-    // Calculate the size up to here and fill the partial shard
-    unsigned int fullSize = (blockchain.chain.size() * blockchain.length * 3) + (blockchain.chain.size() * 2) + 5;
-    blockchain.chain[blockchain.chain.size()].shardHash = data[fullSize - 1];
-    blockchain.chain[blockchain.chain.size()].prevShardHash = data[fullSize];
+    // The unfilled shard (on the end of the blockchain)
+    blockchain.chain[blockchain.chain.size()].shardHash = data[++index];
+    blockchain.chain[blockchain.chain.size()].prevShardHash = data[++index];
 
-    int remainingBlocks = data.size() - fullSize;
-    for(int i = 0; i < remainingBlocks; i++) {
-        blockchain.chain[blockchain.chain.size()].blockHash = data[remainingBlocks + (i * 3) + 1];
-        blockchain.chain[blockchain.chain.size()].prevBlockHash = data[remainingBlocks + (i * 3) + 2];
-        blockchain.chain[blockchain.chain.size()].time = data[remainingBlocks + (i * 3) + 3];
+    // Loop through the remaining blocks in the unfilled shard
+    for(int i = 0; i < blocksRemainingInShard; i++) {
+        blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.keyImage = data[++index];
+        blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.signature = data[++index];
+
+        int blockIndex = ++index;
+        for(int j = 0; j < data[blockIndex]; j++)
+            blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.publicKeys.push_back(data[++index]);
+
+        blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.data.receiverKey = data[++index];
+        blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.data.transactionAmount = data[++index];
+
+        blockchain.chain[blockchain.chain.size()].shard[i].blockHash = data[++index];
+        blockchain.chain[blockchain.chain.size()].shard[i].prevBlockHash = data[++index];
+        blockchain.chain[blockchain.chain.size()].shard[i].time = data[++index];
     }
 }
 
 void SaveBlockchain::read(Blockchain* chain, std::string filepath) {
     std::vector<std::string> data = reader.getData(&filepath);
 
-    chain.id = data[0];                   // The blockchain unique identifier
-    chain.length = stoul(data[1]);        // The shard length
-    chain.numOfShards = stoul(data[2]);   // The number of shards in the chain
+    int blocksRemainingInShard = data[0];      // The number of blocks in the unfilled shard
+    chain.id = data[1];                   // The blockchain unique identifier
+    chain.length = stoul(data[2]);        // The shard length
+    chain.numOfShards = stoul(data[3]);   // The number of shards in the chain
 
-    // numOfShards - 1 to negate the last shard (last shard may not be full)
+    int index = 4;     // The index counter
+
+    // Filled Shards
     for(int i = 1; i <= (chain.numOfShards - 1); i++) {
-        unsigned int index = (chain.length * 3 * i) + (2 * i) + 3;
+        chain.chain[i].shardHash = data[index];
+        chain.chain[i].prevShardHash = data[++index];
 
-        chain.chain[i].shardHash = data[index + 1];
-        chain.chain[i].prevShardHash = data[index + 2];
-
+        // Block
         for(int j = 0; j < chain.length; j++) {
-            int blockIndex = (index + 2) + j * 3;   // start at index + 2 -> (index + 2) + (j * 3) + 1, 2, or 3
-            chain.chain[i].shard[j].blockHash = data[blockIndex + 1];
-            chain.chain[i].shard[j].prevBlockHash = data[blockIndex + 2];
-            chain.chain[i].shard[j].time = data[blockIndex + 3];
+            chain.chain[i].shard[j].ringsignature.keyImage = data[++index];
+            chain.chain[i].shard[j].ringsignature.signature = data[++index];
+
+            int blockIndex = ++index;
+            for(int k = 0; k < data[blockIndex]; k++)   // Ring of public keys here
+                chain.chain[i].shard[j].ringsignature.publicKeys.push_back(data[++index]);
+
+            chain.chain[i].shard[j].ringsignature.data.receiverKey = data[++index];
+            chain.chain[i].shard[j].ringsignature.data.transactionAmount = stoul(data[++index]);
+
+            chain.chain[i].shard[j].blockHash = data[++index];
+            chain.chain[i].shard[j].prevBlockHash = data[++index];
+            chain.chain[i].shard[j].time = data[++index];
         }
     }
 
-    // Calculate the size up to here and fill the partial shard
-    unsigned int fullSize = (chain.chain.size() * chain.length * 3) + (chain.chain.size() * 2) + 5;
-    chain.chain[chain.chain.size()].shardHash = data[fullSize - 1];
-    chain.chain[chain.chain.size()].prevShardHash = data[fullSize];
+    // The unfilled shard (on the end of the blockchain)
+    chain.chain[chain.chain.size()].shardHash = data[++index];
+    chain.chain[chain.chain.size()].prevShardHash = data[++index];
 
-    int remainingBlocks = data.size() - fullSize;
-    for(int i = 0; i < remainingBlocks; i++) {
-        chain.chain[chain.chain.size()].blockHash = data[remainingBlocks + (i * 3) + 1];
-        chain.chain[chain.chain.size()].prevBlockHash = data[remainingBlocks + (i * 3) + 2];
-        chain.chain[chain.chain.size()].time = data[remainingBlocks + (i * 3) + 3];
+    // Loop through the remaining blocks in the unfilled shard
+    for(int i = 0; i < blocksRemainingInShard; i++) {
+        chain.chain[chain.chain.size()].shard[i].ringsignature.keyImage = data[++index];
+        chain.chain[chain.chain.size()].shard[i].ringsignature.signature = data[++index];
+
+        int blockIndex = ++index;
+        for(int j = 0; j < data[blockIndex]; j++)
+            chain.chain[chain.chain.size()].shard[i].ringsignature.publicKeys.push_back(data[++index]);
+
+        chain.chain[chain.chain.size()].shard[i].ringsignature.data.receiverKey = data[++index];
+        chain.chain[chain.chain.size()].shard[i].ringsignature.data.transactionAmount = data[++index];
+
+        chain.chain[chain.chain.size()].shard[i].blockHash = data[++index];
+        chain.chain[chain.chain.size()].shard[i].prevBlockHash = data[++index];
+        chain.chain[chain.chain.size()].shard[i].time = data[++index];
     }
 }
 
 void SaveBlockchain::save() {
     std::vector<std::string> data;
 
+    int blocksRemainingInShard = blockchain.chain.back().shard.size();
     data.push_back(blockchain.id);
     data.push_back(std::to_string(blockchain.length));
     data.push_back(std::to_string(blockchain.numOfShards));
 
+    // Filled Shards
     for(int i = 1; i <= (blockchain.numOfShards - 1); i++) {
-        unsigned int index = (blockchain.length * 3 * i) + (2 * i) + 3;
+        data.push_back(blockchain.chain[i].shardHash);
+        data.push_back(blockchain.chain[i].prevShardHash);
 
-        data[index + 1] = blockchain.chain[i].shardHash;
-        data[index + 2] = blockchain.chain[i].prevShardHash;
-
+        // Block
         for(int j = 0; j < blockchain.length; j++) {
-            int blockIndex = (index + 2) + j * 3;   // start at index + 2 -> (index + 2) + (j * 3) + 1, 2, or 3
-            data[blockIndex + 1] = blockchain.chain[i].shard[j].blockHash;
-            data[blockIndex + 2] = blockchain.chain[i].shard[j].prevBlockHash;
-            data[blockIndex + 3] = blockchain.chain[i].shard[j].time;
+            data.push_back(blockchain.chain[i].shard[j].ringsignature.keyImage);
+            data.push_back(blockchain.chain[i].shard[j].ringsignature.signature);
+
+            data.push_back(blockchain.chain[i].shard[j].ringsignature.publicKeys.size());
+            for(int k = 0; k < blockchain.chain[i].shard[j].ringsignature.publicKeys.size(); k++)   // Ring of public keys here
+                data.push_back(blockchain.chain[i].shard[j].ringsignature.publicKeys[k]);
+
+            data.push_back(blockchain.chain[i].shard[j].ringsignature.data.receiverKey);
+            data.push_back(std::to_string(blockchain.chain[i].shard[j].ringsignature.data.transactionAmount));
+
+            data.push_back(blockchain.chain[i].shards[j].blockHash);
+            data.push_back(blockchain.chain[i].shards[j].prevBlockHash);
+            data.push_back(blockchain.chain[i].shards[j].time);
         }
     }
 
-    unsigned int fullSize = (blockchain.chain.size() * blockchain.length * 3) + (blockchain.chain.size() * 2) + 5;
-    data[fullSize - 1] = blockchain.chain[blockchain.chain.size()].shardHash;
-    data[fullSize] = blockchain.chain[blockchain.chain.size()].prevShardHash;
+    // The unfilled shard (on the end of the blockchain)
+    data.push_back(blockchain.chain[blockchain.chain.size()].shardHash);
+    data.push_back(blockchain.chain[blockchain.chain.size()].prevShardHash);
 
-    int remainingBlocks = ((blockchain.chain.size() * 2) + (blockchain.chain.size() * 3 * blockchain.length) + 3) - fullSize;
-    for(int i = 0; i < remainingBlocks; i++) {
-        data[remainingBlocks + (i * 3) + 1] = blockchain.chain[blockchain.chain.size()].blockHash;
-        data[remainingBlocks + (i * 3) + 2] = blockchain.chain[blockchain.chain.size()].prevBlockHash;
-        data[remainingBlocks + (i * 3) + 3] = blockchain.chain[blockchain.chain.size()].time;
+    // Loop through the remaining blocks in the unfilled shard
+    for(int i = 0; i < blocksRemainingInShard; i++) {
+        data.push_back(blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.keyImage);
+        data.push_back(blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.signature);
+
+        for(int j = 0; j < blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.publicKeys.size(); j++)
+            data.push_back(blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.publicKeys[j]);
+
+        data.push_back(blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.data.receiverKey);
+        data.push_back(std::to_string(blockchain.chain[blockchain.chain.size()].shard[i].ringsignature.data.transactionAmount));
+
+        data.push_back(blockchain.chain[blockchain.chain.size()].shard[i].blockHash);
+        data.push_back(blockchain.chain[blockchain.chain.size()].shard[i].prevBlockHash);
+        data.push_back(blockchain.chain[blockchain.chain.size()].shard[i].time);
     }
 
     writer.createFile(data, &path);
@@ -114,33 +173,52 @@ void SaveBlockchain::save() {
 void SaveBlockchain::save(Blockchain* chain, std::string filepath) {
     std::vector<std::string> data;
 
+    int blocksRemainingInShard = chain.chain.back().shard.size();
     data.push_back(chain.id);
     data.push_back(std::to_string(chain.length));
     data.push_back(std::to_string(chain.numOfShards));
 
+    // Filled Shards
     for(int i = 1; i <= (chain.numOfShards - 1); i++) {
-        unsigned int index = (chain.length * 3 * i) + (2 * i) + 3;
+        data.push_back(chain.chain[i].shardHash);
+        data.push_back(chain.chain[i].prevShardHash);
 
-        data[index + 1] = chain.chain[i].shardHash;
-        data[index + 2] = chain.chain[i].prevShardHash;
-
+        // Block
         for(int j = 0; j < chain.length; j++) {
-            int blockIndex = (index + 2) + j * 3;   // start at index + 2 -> (index + 2) + (j * 3) + 1, 2, or 3
-            data[blockIndex + 1] = chain.chain[i].shard[j].blockHash;
-            data[blockIndex + 2] = chain.chain[i].shard[j].prevBlockHash;
-            data[blockIndex + 3] = chain.chain[i].shard[j].time;
+            data.push_back(chain.chain[i].shard[j].ringsignature.keyImage);
+            data.push_back(chain.chain[i].shard[j].ringsignature.signature);
+
+            data.push_back(chain.chain[i].shard[j].ringsignature.publicKeys.size());
+            for(int k = 0; k < chain.chain[i].shard[j].ringsignature.publicKeys.size(); k++)   // Ring of public keys here
+                data.push_back(chain.chain[i].shard[j].ringsignature.publicKeys[k]);
+
+            data.push_back(chain.chain[i].shard[j].ringsignature.data.receiverKey);
+            data.push_back(std::to_string(chain.chain[i].shard[j].ringsignature.data.transactionAmount));
+
+            data.push_back(chain.chain[i].shards[j].blockHash);
+            data.push_back(chain.chain[i].shards[j].prevBlockHash);
+            data.push_back(chain.chain[i].shards[j].time);
         }
     }
 
-    unsigned int fullSize = (chain.chain.size() * chain.length * 3) + (chain.chain.size() * 2) + 5;
-    data[fullSize - 1] = chain.chain[chain.chain.size()].shardHash;
-    data[fullSize] = chain.chain[chain.chain.size()].prevShardHash;
+    // The unfilled shard (on the end of the blockchain)
+    data.push_back(chain.chain[chain.chain.size()].shardHash);
+    data.push_back(chain.chain[chain.chain.size()].prevShardHash);
 
-    int remainingBlocks = ((chain.chain.size() * 2) + (chain.chain.size() * 3 * chain.length) + 3) - fullSize;
-    for(int i = 0; i < remainingBlocks; i++) {
-        data[remainingBlocks + (i * 3) + 1] = chain.chain[chain.chain.size()].blockHash;
-        data[remainingBlocks + (i * 3) + 2] = chain.chain[chain.chain.size()].prevBlockHash;
-        data[remainingBlocks + (i * 3) + 3] = chain.chain[chain.chain.size()].time;
+    // Loop through the remaining blocks in the unfilled shard
+    for(int i = 0; i < blocksRemainingInShard; i++) {
+        data.push_back(chain.chain[chain.chain.size()].shard[i].ringsignature.keyImage);
+        data.push_back(chain.chain[chain.chain.size()].shard[i].ringsignature.signature);
+
+        for(int j = 0; j < chain.chain[chain.chain.size()].shard[i].ringsignature.publicKeys.size(); j++)
+            data.push_back(chain.chain[chain.chain.size()].shard[i].ringsignature.publicKeys[j]);
+
+        data.push_back(chain.chain[chain.chain.size()].shard[i].ringsignature.data.receiverKey);
+        data.push_back(std::to_string(chain.chain[chain.chain.size()].shard[i].ringsignature.data.transactionAmount));
+
+        data.push_back(chain.chain[chain.chain.size()].shard[i].blockHash);
+        data.push_back(chain.chain[chain.chain.size()].shard[i].prevBlockHash);
+        data.push_back(chain.chain[chain.chain.size()].shard[i].time);
     }
 
     writer.createFile(data, &filepath);
