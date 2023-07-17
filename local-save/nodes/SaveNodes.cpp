8d0a6ee0c633e9be974e6c02b2b5d4fdbcb83509
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-17 14:37:50 +1000

    Added Node Bias to Local-Save
    Added a node bias parameter to the local save framework (now each node is using Position4D instead of Position3D)

--- local-save/nodes/SaveNodes.cpp
+++ local-save/nodes/SaveNodes.cpp
@@ -1,6 +1,10 @@
 #include "SaveNodes.h"
 
 
+/* x: Public Address
+    y: IP Address
+    z: Port
+    i: Bias */
 SaveNodes::SaveNodes(std::string filepath) {
     read(filepath);
 }
@@ -9,23 +13,25 @@ void SaveNodes::read(std::string filepath) {
     path = filepath;
     std::vector<std::string> data = reader.getData(&path);
 
-    Position3D temp;
-    for(int i = 0; i < data.size() / 3; i++) {
-        temp.x = data[i * 3];                 // Node Public Address
-        temp.y = data[(i * 3) + 1];           // Node IP Address
-        temp.z = stoi(data[(i * 3) + 2]);     // Node Port
+    Position4D temp;
+    for(int i = 0; i < data.size() / 4; i++) {
+        temp.x = data[i * 3];                   // Node Public Address
+        temp.y = data[(i * 3) + 1];             // Node IP Address
+        temp.z = stoi(data[(i * 3) + 2]);       // Node Port
+        temp.i = stoull(data[(i * 3) + 3]);    // Node Bias
         nodes.push_back(temp);
     }
 }
 
-void SaveNodes::read(std::vector<Position3D>* n, std::string filepath) {
+void SaveNodes::read(std::vector<Position4D>* n, std::string filepath) {
     std::vector<std::string> data = reader.getData(&filepath);
 
-    Position3D temp;
-    for(int i = 0; i < data.size() / 3; i++) {
-        temp.x = data[i * 3];                 // Node Public Address
-        temp.y = data[(i * 3) + 1];           // Node IP Address
-        temp.z = stoi(data[(i * 3) + 2]);     // Node Port
+    Position4D temp;
+    for(int i = 0; i < data.size() / 4; i++) {
+        temp.x = data[i * 3];                   // Node Public Address
+        temp.y = data[(i * 3) + 1];             // Node IP Address
+        temp.z = stoi(data[(i * 3) + 2]);       // Node Port
+        temp.i = stoull(data[(i * 3) + 3]);    // Node Bias
         n.push_back(temp);
     }
 }
@@ -37,18 +43,20 @@ void SaveNodes::save() {
         data.push_back(nodes[i].x);
         data.push_back(nodes[i].y);
         data.push_back(std::to_string(nodes[i].z));
+        data.push_back(std::to_string(nodes[i].i));
     }
 
     writer.createFile(data, &path);
 }
 
-void SaveNodes::save(std::vector<Position3D>* n, std::string filepath) {
+void SaveNodes::save(std::vector<Position4D>* n, std::string filepath) {
     std::vector<std::string> data;
 
     for(int i = 0; i < n.size(); i++) {
         data.push_back(n[i].x);
         data.push_back(n[i].y);
         data.push_back(std::to_string(n[i].z));
+        data.push_back(std::to_string(n[i].i));
     }
 
     writer.createFile(data, &filepath);
--- local-save/nodes/SaveNodes.h
+++ local-save/nodes/SaveNodes.h
@@ -11,16 +11,16 @@ struct SaveNodes {
     FileReader reader;
 
     std::string path;                  // The path to the nodes.vlnc file
-    std::vector<Position3D> nodes;     // The nodes vector that is stored in the nodes.vlnc file
+    std::vector<Position4D> nodes;     // The nodes vector that is stored in the nodes.vlnc file
 
     // Set the path, read the file, decode the file into the nodes vector
     SaveNodes() {};                     // Make sure not to call save() before reading a file!
     SaveNodes(std::string filepath);    // Reads the nodes on construction
 
     void read(std::string filepath);                                // Reads the nodes - stores nodes + filepath locally
-    void read(std::vector<Position3D>* n, std::string filepath);    // Reads the nodes - stores nodes at pointer
+    void read(std::vector<Position4D>* n, std::string filepath);    // Reads the nodes - stores nodes at pointer
     void save();                                                    // Saves the nodes to the filepath (reverse of reading the file)
-    void save(std::vector<Position3D>* n, std::string filepath);    // Saves the nodes at the input pointer to the inputted filepath
+    void save(std::vector<Position4D>* n, std::string filepath);    // Saves the nodes at the input pointer to the inputted filepath
 };
 
 #endif
