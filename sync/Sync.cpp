c001c5e6050a88cd9ea2ff68b8f7b92c43b4bc3f
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-18 13:52:46 +1000

    Removed Active Node Syncing
    Removed the active node syncing function from the high-level syncing framework

--- sync/Sync.cpp
+++ sync/Sync.cpp
@@ -1,24 +1,21 @@
 #include "Sync.h"
 
 
-Sync::Sync(bool node, std::string blockchainPath, std::string activeNodesPath, std::string knownNodesPath) {
+Sync::Sync(bool node, std::string blockchainPath, std::string knownNodesPath) {
     // Set file paths
     blockchainFilepath = blockchainPath;
-    activeNodesFilepath = activeNodesPath;
     knownNodesFilepath = knownNodesPath;
 
     // Read each file
-    readBlockchain(); readActiveNodes(); readKnownNodes();
+    readBlockchain(); readKnownNodes();
 
     // Continuously Sync (these open in new threads)
     switch(node) {
     case 0:
-        syncActiveNodes.sync(activeNodes, knownNodes);
         syncKnownNodes.sync(knownNodes, activeNodes);
         syncBlockchain.sync(blockchain, activeNodes);
         break;
     case 1:
-        syncActiveNodes.nodeSync(activeNodes, knownNodes);
         syncKnownNodes.nodeSync(knownNodes, activeNodes);
         syncBlockchain.nodeSync(blockchain, activeNodes);
         break;
@@ -26,11 +23,9 @@ Sync::Sync(bool node, std::string blockchainPath, std::string activeNodesPath, s
     
 }
 
-Sync::~Sync() { saveBlockchain(); saveBlockchain(); saveBlockchain(); }     // Save the data to file
+Sync::~Sync() { saveBlockchain(); saveKnownNodes(); }     // Save the data to file
 
 void Sync::readBlockchain() { syncBlockchain.read(blockchain, blockchainFilepath); }
-void Sync::readActiveNodes() { syncActiveNodes.read(activeNodes, activeNodesFilepath); }
 void Sync::readKnownNodes() { syncKnownNodes.read(knownNodes, knownNodesFilepath); }
 void Sync::saveBlockchain() { syncBlockchain.save(blockchain, blockchainFilepath); }
-void Sync::saveActiveNodes() { syncActiveNodes.save(activeNodes, activeNodesFilepath); }
 void Sync::saveKnownNodes() { syncKnownNodes.save(knownNodes, knownNodesFilepath); }
