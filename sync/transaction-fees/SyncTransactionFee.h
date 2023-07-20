ea51ce5fe7cd9635edf63bc7e8cc758aa6d472dc
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-20 21:53:30 +1000

    Added Getter Functions
    Added getter functions to the synctransactionfee  framework header

--- sync/transaction-fees/SyncTransactionFee.h
+++ sync/transaction-fees/SyncTransactionFee.h
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <vector>
+#include <thread>
+#include "valency-core/custom-types/Position.h"
 #include "valency-core/networking/client/Client.h"
 #include "valency-core/networking/server/Server.h"
 #include "valency-core/networking/onion-routing/Onion.h"
@@ -14,16 +16,20 @@
         -> Updates the transaction fee IF a new block is added AND the previous transaction fee has expired(timed out) */
 class SyncTransactionFee {
 private:
-    std::vector<std::pair<std::string, double>>* transactionfees;   // Contains all transaction fee's for every combination of #ofTransactions & #ofOnionRoutingNodes
+    std::vector<double> transactionfees;   // Contains all transaction fee's for every combination of #ofTransactions & #ofOnionRoutingNodes
+    std::string expiry;                    // Contains the expiry time for the transaction fee's
 
     Server server;
     Client client;
     Onion onion(2);
 
 public:
-    SyncTransactionFee();   // Creates and runs its own thread
+    SyncTransactionFee(std::vector<Position4D>* knownNodes);   // Creates and runs its own thread doing sync()
 
     void sync(std::vector<Position4D>* knownNodes);     // Requires Knownnodes list to get the number of active nodes (used to determine network load and therefore fee's)
+    
+    std::pair<std::string, double> getTransactionFee(int numOfTransactions);                                            // Returns the transaction fee and expiry
+    std::pair<std::string, double> getTransactionFee(int numOfTransactions, bool onionRouting, int numOfOnionNodes);    // Returns the transaction fee and expiry
 };
 
 #endif
