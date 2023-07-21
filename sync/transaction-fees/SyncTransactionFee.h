47dd414f2e8e1863e67e247031f27aa293ea4671
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-21 18:28:24 +1000

    Added Updated Header
    Updated the SyncTransactionFee Header - mainly added separate sync functions and splitting the expiry (individual) and fee's (vector)

--- sync/transaction-fees/SyncTransactionFee.h
+++ sync/transaction-fees/SyncTransactionFee.h
@@ -1,9 +1,13 @@
 #ifndef SYNCTRANSACTIONFEE_H
 #define SYNCTRANSACTIONFEE_H
 
+#include <ctime>
+#include <chrono>
 #include <string>
 #include <vector>
 #include <thread>
+#include <sstream>
+#include <iomanip>
 #include "valency-core/custom-types/Position.h"
 #include "valency-core/networking/client/Client.h"
 #include "valency-core/networking/server/Server.h"
@@ -17,19 +21,21 @@
 class SyncTransactionFee {
 private:
     std::vector<double> transactionfees;   // Contains all transaction fee's for every combination of #ofTransactions & #ofOnionRoutingNodes
-    std::string expiry;                    // Contains the expiry time for the transaction fee's
+    std::stringstream expiry;              // Contains the expiry time for the transaction fee's (do expiry.str() to get string version)
 
     Server server;
     Client client;
     Onion onion(2);
+
+    void syncExpiry();    // Syncs the expiry time - this increments by 30s every 30s - doesn't need to get current expiry from other nodes because it just increments by 30s every 30s (starting from 0s)
 
 public:
     SyncTransactionFee(std::vector<Position4D>* knownNodes);   // Creates and runs its own thread doing sync()
 
     void sync(std::vector<Position4D>* knownNodes);     // Requires Knownnodes list to get the number of active nodes (used to determine network load and therefore fee's)
     
-    std::pair<std::string, double> getTransactionFee(int numOfTransactions);                                            // Returns the transaction fee and expiry
-    std::pair<std::string, double> getTransactionFee(int numOfTransactions, bool onionRouting, int numOfOnionNodes);    // Returns the transaction fee and expiry
+    std::pair<std::string, double> getTransactionFee(int numOfTransactions);                         // Returns the transaction fee and expiry
+    std::pair<std::string, double> getTransactionFee(int numOfTransactions, int numOfOnionNodes);    // Returns the transaction fee and expiry
 };
 
 #endif
