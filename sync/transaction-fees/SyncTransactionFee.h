3f78cca46f9dcb8d66412b3c7a3b85fd7b426d95
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-22 19:49:43 +1000

    Updated Header
    Added an updated transaction fee syncing header (removed useless networking stuffs, moved thread creation from constructor into other functions)

--- sync/transaction-fees/SyncTransactionFee.h
+++ sync/transaction-fees/SyncTransactionFee.h
@@ -8,10 +8,9 @@
 #include <thread>
 #include <sstream>
 #include <iomanip>
+#include "configuration/Configuration.h"
 #include "valency-core/custom-types/Position.h"
 #include "valency-core/networking/client/Client.h"
-#include "valency-core/networking/server/Server.h"
-#include "valency-core/networking/onion-routing/Onion.h"
 
 
 /* Analyses Chain and Network Data to generate transaction fees
@@ -23,19 +22,18 @@ private:
     std::vector<double> transactionfees;   // Contains all transaction fee's for every combination of #ofTransactions & #ofOnionRoutingNodes
     std::stringstream expiry;              // Contains the expiry time for the transaction fee's (do expiry.str() to get string version)
 
-    Server server;
     Client client;
-    Onion onion(2);
+    Configuration config;
 
-    void syncExpiry();    // Syncs the expiry time - this increments by 30s every 30s - doesn't need to get current expiry from other nodes because it just increments by 30s every 30s (starting from 0s)
+    void syncExpiry();                                     // Syncs the expiry time - this increments by 30s every 30s - doesn't need to get current expiry from other nodes because it just increments by 30s every 30s (starting from 0s)
+    void syncFee(std::vector<Position4D>* knownNodes);     // Syncs the transaction fee - requires Knownnodes list to get the number of active nodes (used to determine network load and therefore fee's)
 
 public:
-    SyncTransactionFee(std::vector<Position4D>* knownNodes);   // Creates and runs its own thread doing sync()
+    SyncTransactionFee() {};   
 
-    void sync(std::vector<Position4D>* knownNodes);     // Requires Knownnodes list to get the number of active nodes (used to determine network load and therefore fee's)
+    void sync(std::vector<Position4D>* knownNodes);     // Creates and runs its own thread doing sync()   
     
     std::pair<std::string, double> getTransactionFee(int numOfTransactions);                         // Returns the transaction fee and expiry
-    std::pair<std::string, double> getTransactionFee(int numOfTransactions, int numOfOnionNodes);    // Returns the transaction fee and expiry
 };
 
 #endif
