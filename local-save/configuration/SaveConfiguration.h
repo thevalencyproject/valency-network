a787ad1b614e3c07ac635d7afccda06660475246
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-23 20:26:36 +1000

    Added Configuration Save Framework Header
    Added a header for the configuration file reading/writing framework - implementation coming soon!

---
+++ local-save/configuration/SaveConfiguration.h
@@ -0,0 +1,26 @@
+#ifndef SAVECONFIGURATION_H
+#define SAVECONFIGURATION_H
+
+#include <string>
+#include "valency-core/file-recognition/file-writer/FileWriter.h"
+#include "valency-core/file-recognition/file-reader/FileReader.h"
+
+
+struct SaveConfiguration {
+    FileWriter writer;
+    FileReader reader;
+
+    std::string path;   // The path to the configuration.vlnc file
+    Configuration configuration;    // The configuration that is stored in the configuration.vlnc file
+
+    // Set the path, read the file, decode the file into the configuration object
+    SaveConfiguration() {};                     // Make sure not to call save() before reading a file!
+    SaveConfiguration(std::string filepath);    // Reads the configuration on construction
+
+    void read(std::string filepath);                                // Reads the configuration - stores configuration + filepath locally
+    void read(Configuration* n, std::string filepath);    // Reads the configuration - stores configuration at pointer
+    void save();                                                    // Saves the configuration to the filepath (reverse of reading the file)
+    void save(Configuration* n, std::string filepath);    // Saves the configuration at the input pointer to the inputted filepath
+};
+
+#endif
