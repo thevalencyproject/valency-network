228011ecc5306e9fa6ba37b1f968c6d864a59380
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-24 22:38:23 +1000

    Added a Node Structure
    Added a structure for other nodes on the network (keeping knownnode syncing vectors such as other node details, other nodes stake, other nodes bandwidth, and unverified blocks in the one structure)

--- configuration/Configuration.h
+++ configuration/Configuration.h
@@ -12,11 +12,6 @@ struct Configuration {
     int minNumOfNodes = 3;
     int minNumOfOnionNodes = 5;
     int maxNumOfOnionNodes = 6;
-
-    // Dynamic - some are set to fixed values - this will be updated in later revisions
-    unsigned int networkLoad = 10;    // 1 -> 1000000, with 1 being the least load
-
-    void detectLoad();  // Creates its own thread and constantly updates the networkload variable
 };
 
 #endif
