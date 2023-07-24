ce84ef7ea02b18546a10e9e9feffd5fdf42d86f6
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-24 22:53:18 +1000

    Made the Bias Framework use OtherNodes struct
    made the bias framework use the othernodes struct!

--- bias/Bias.cpp
+++ bias/Bias.cpp
@@ -1,13 +1,13 @@
 #include "Bias.h"
 
 
-Bias::Bias(Blockchain* blockchain, std::vector<Position4D>* nodes) {
+Bias::Bias(Blockchain* blockchain, std::vector<OtherNodes>* nodes) {
     while(1)
         std::thread bias(refreshBias(blockchain, nodes));   // Constantly refresh biases
 }
 
 // So far, this bias function runs 100% on proof-of-stake, however, bias will be added based on node bandwidth (this will be dynamic like the whitepaper states)
-void Bias::refreshBias(Blockchain* blockchain, std::vector<Position4D>* nodes) {
+void Bias::refreshBias(Blockchain* blockchain, std::vector<OtherNodes>* nodes) {
     for(int i = 0; i < blockchain->numOfShards; i++) {   // Loop through each shard in the blockchain
         for(int j = 0; j < blockchain->length; j++) {    // Loop through each block in each shard
 
@@ -17,15 +17,15 @@ void Bias::refreshBias(Blockchain* blockchain, std::vector<Position4D>* nodes) {
                 // Search that block to see if a node has sent VLNC to stake
                 for(int k = 0; k < nodes.size(); k++) {
                     // If the transaction is sending VLNC to the node address, that is adding to the staked amount (bias) for the node
-                    if(blockchain->chain[i].shard[j].signature.publicKeys[0] == nodes[k].x) {
+                    if(blockchain->chain[i].shard[j].signature.publicKeys[0] == nodes[k].details.address) {
                         // Add transaction amount to bias
-                        nodes[k].i = nodes[k].i + blockchain->chain[i].shard[j].signature.data.transactionAmount;
+                        nodes[k].details.bias = nodes[k].details.bias + blockchain->chain[i].shard[j].signature.data.transactionAmount;
                     }
 
                     // If the transaction is sending VLNC from the node address, that is subtracting from the staked amount (bias) for the node
                     if(blockchain->chain[i].shard[j].signature.publicKeys[1] == nodes[k].x) {      
                         // Subtract transaction amount from bias
-                        nodes[k].i = nodes[k].i - blockchain->chain[i].shard[j].signature.data.transactionAmount;
+                        nodes[k].details.bias = nodes[k].details.bias - blockchain->chain[i].shard[j].signature.data.transactionAmount;
                     }
                 }
             }
@@ -35,7 +35,7 @@ void Bias::refreshBias(Blockchain* blockchain, std::vector<Position4D>* nodes) {
     std::this_thread::sleep_for(std::chrono::milliseconds(5));  // Pause for 5ms
 }
 
-void Bias::refreshBias(Blockchain* blockchain, std::string nodePublicAddress, std::vector<Position4D>* nodes) {
+void Bias::refreshBias(Blockchain* blockchain, std::string nodePublicAddress, std::vector<OtherNodes>* nodes) {
     unsigned int bias;
     for(int i = 0; i < blockchain->numOfShards; i++) {   // Loop through each shard in the blockchain
         for(int j = 0; j < blockchain->length; j++) {    // Loop through each block in each shard
@@ -57,12 +57,12 @@ void Bias::refreshBias(Blockchain* blockchain, std::string nodePublicAddress, st
 
     // Get the bias of the node and find add bias to it
     for(int i = 0; i < nodes.size(); i++)
-        if(nodePublicAddress == nodes[i].x)
-            nodes[i].i = nodes[i].i + bias;
+        if(nodePublicAddress == nodes[i].details.address)
+            nodes[i].details.bias = nodes[i].details.bias + bias;
 }
 
-unsigned long Bias::getBias(std::string nodePublicAddress, std::vector<Position4D> nodes) {
+unsigned long Bias::getBias(std::string nodePublicAddress, std::vector<OtherNodes> nodes) {
     for(int i = 0; i < nodes.size(); i++)
-        if(nodePublicAddress == nodes[i].x)
-            return nodes[i].i;
+        if(nodePublicAddress == nodes[i].details.address)
+            return nodes[i].details.bias;
 }
--- bias/Bias.h
+++ bias/Bias.h
@@ -4,22 +4,19 @@
 #include <string>
 #include <vector>
 #include <thread>
+#include "structures/Node.h"
 #include "structures/Blockchain.h"
 #include "valency-core/custom-types/Position.h"
 
 
 struct Bias {
-private:    // A VECTOR OF UNSIGNED SHORT COULD BE ADDED INTO THE KNOWNNODES VECTOR - this means it would be automatically synced!
-    std::vector<std::vector<unsigned short>>* nodeBandwidth     // Contains each node's interpretation of how much bandwidth every other node has (index corresponds with knownnodes index)
-                                                                //      -> This should be resized to the size of the knownnodes vector!
-public:
     // Whenever the constructor is called, a thread is created and refreshBias is run constantly
-    Bias(Blockchain* blockchain, std::vector<Position4D>* nodes);
+    Bias(Blockchain* blockchain, std::vector<OtherNodes>* nodes);
     
-    void refreshBias(Blockchain* blockchain, std::vector<Position4D>* nodes);                                   // Refresh all node biases - done by going through the blockchain and looking for staked VLNC
-    void refreshBias(Blockchain* blockchain, std::string nodePublicAddress, std::vector<Position4D>* nodes);    // Refreshes a single node bias - done by going through the blockchain and looking for staked VLNC from the node public address
+    void refreshBias(Blockchain* blockchain, std::vector<OtherNodes>* nodes);                                   // Refresh all node biases - done by going through the blockchain and looking for staked VLNC
+    void refreshBias(Blockchain* blockchain, std::string nodePublicAddress, std::vector<OtherNodes>* nodes);    // Refreshes a single node bias - done by going through the blockchain and looking for staked VLNC from the node public address
     
-    unsigned long getBias(std::string nodePublicAddress, std::vector<Position4D>* nodes);    // Gets the bias of a single node
+    unsigned long getBias(std::string nodePublicAddress, std::vector<OtherNodes>* nodes);    // Gets the bias of a single node
     void setBandwidth(std::vector<std::vector<unsigned short>>* nodeBandwidth);               // Changes the bias according to the input (this is run in a second thread)
 };
 
