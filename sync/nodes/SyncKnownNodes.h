a3b0b1040e5c1454d1f78e937e263e0ad7892485
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-25 23:30:08 +1000

    Updated to NodeInfo
    Updated from Position4D to NodeInfo

--- sync/nodes/SyncKnownNodes.h
+++ sync/nodes/SyncKnownNodes.h
@@ -2,6 +2,7 @@
 #define SYNCKNOWNNODES_H
 
 #include <thread>
+#include "structures/Node.h"
 #include "local-save/nodes/SaveNodes.h"
 #include "configuration/Configuration.h"
 #include "valency-core/custom-types/Position.h"
@@ -16,14 +17,15 @@ private:
     /* x: Public Address
        y: IP Address
        z: Port
-       i: Bias */
-    std::vector<Position4D>* knownnodes;
+       i: Bias
+       j: Network Node Bandwidth Vector */
+    std::vector<NodeDetails>* knownnodes;
     Configuration* configuration;                  // A pointer to the network configuration
 
     int numberOfDefaultNodes = 5;                                                       // The number of nodes that are included in the program by default
     unsigned int numOfActiveNodes;                                                      // The number of nodes that are active on the network
     unsigned long bias = 0;                                                             // Node Bias - replicating mutex without mutex
-    std::vector<std::pair<std::vector<Position4D>, unsigned long>> unverifiedNodes;     // The knownnodes that some nodes have sent, but others havent - unsigned long is the node bias
+    std::vector<std::pair<std::vector<NodeDetails>, unsigned long>> unverifiedNodes;     // The knownnodes that some nodes have sent, but others havent - unsigned long is the node bias
     void validate();                                                                    // Validates the incoming knownnodes
 
     Server server;
@@ -32,10 +34,10 @@ private:
     SaveNodes save;
     LZMACompression lzma;
 
-    std::string convertNode(Position4D* node);                  // Converts a node into string form - used for network communication
-    std::string convertNode(std::vector<Position4D>* nodes);    // Converts a vector of nodes into string form - used for network communication
-    Position4D convertString(std::string node);                 // Converts string to a node (string should come from convertNode() function)
-    std::vector<Position4D> convertString(std::string nodes);   // Converts string to a vector of nodes (string should come from convertNode() function)
+    std::string convertNode(NodeDetails* node);                  // Converts a node into string form - used for network communication
+    std::string convertNode(std::vector<NodeDetails>* nodes);    // Converts a vector of nodes into string form - used for network communication
+    NodeDetails convertString(std::string node);                 // Converts string to a node (string should come from convertNode() function)
+    std::vector<NodeDetails> convertString(std::string nodes);   // Converts string to a vector of nodes (string should come from convertNode() function)
 
     // Interfaces with the client networking framework to connect to a node and increment numOfActiveNodes
     template<typename T>
@@ -52,13 +54,13 @@ public:
     // Runs in its own core - constantly syncs the known nodes: knownNodes should contain atleast 1 active node
     //   -> Designed to take pointers to constantly changing vectors (changed in other threads)
     //   -> sync() is for wallets + misc programs and nodeSync() is for nodes
-    void sync(std::vector<Position4D>* knownNodes);                                      // Non Onion-Routing
-    void sync(std::vector<Position4D>* knownNodes, std::vector<NodeInfo> nodes);         // Onion-Routing - ensure nodes vector only contains onion-routing nodes and not destination server
-    void nodeSync(std::vector<Position4D>* knownNodes);                                  // Non Onion-Routing
-    void nodeSync(std::vector<Position4D>* knownNodes, std::vector<NodeInfo> nodes);     // Onion-Routing - ensure nodes vector only contains onion-routing nodes and not destination server
+    void sync(std::vector<NodeDetails>* knownNodes);                                      // Non Onion-Routing
+    void sync(std::vector<NodeDetails>* knownNodes, std::vector<NodeInfo> nodes);         // Onion-Routing - ensure nodes vector only contains onion-routing nodes and not destination server
+    void nodeSync(std::vector<NodeDetails>* knownNodes);                                  // Non Onion-Routing
+    void nodeSync(std::vector<NodeDetails>* knownNodes, std::vector<NodeInfo> nodes);     // Onion-Routing - ensure nodes vector only contains onion-routing nodes and not destination server
 
-    void read(std::vector<Position4D>* knownNodes, std::string filePath);    // Also has a file reading/writing functionality through the local-save framework (in valency-network)
-    void save(std::vector<Position4D>* knownNodes, std::string filePath);    //   -> filePath should contain the file name / SaveNodes save;
+    void read(std::vector<NodeDetails>* knownNodes, std::string filePath);    // Also has a file reading/writing functionality through the local-save framework (in valency-network)
+    void save(std::vector<NodeDetails>* knownNodes, std::string filePath);    //   -> filePath should contain the file name / SaveNodes save;
 };
 
 #endif
