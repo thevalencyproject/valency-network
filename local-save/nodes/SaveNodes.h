e2755a8bc181836555a75e23ffc690588be4ea7d
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-25 19:45:15 +1000

    Updated NodeDetails SaveNodes Header
    Replaced Position4D with NodeDetails in the savenodes header

--- local-save/nodes/SaveNodes.h
+++ local-save/nodes/SaveNodes.h
@@ -1,6 +1,7 @@
 #ifndef SAVENODES_H
 #define SAVENODES_H
 
+#include "/structures/Node.h"
 #include "valency-core/custom-types/Position.h"
 #include "valency-core/file-recognition/file-writer/FileWriter.h"
 #include "valency-core/file-recognition/file-reader/FileReader.h"
@@ -10,17 +11,17 @@ struct SaveNodes {
     FileWriter writer;
     FileReader reader;
 
-    std::string path;                  // The path to the nodes.vlnc file
-    std::vector<Position4D> nodes;     // The nodes vector that is stored in the nodes.vlnc file
+    std::string path;                   // The path to the nodes.vlnc file
+    std::vector<NodeDetails> nodes;     // The nodes vector that is stored in the nodes.vlnc file
 
     // Set the path, read the file, decode the file into the nodes vector
     SaveNodes() {};                     // Make sure not to call save() before reading a file!
     SaveNodes(std::string filepath);    // Reads the nodes on construction
 
-    void read(std::string filepath);                                // Reads the nodes - stores nodes + filepath locally
-    void read(std::vector<Position4D>* n, std::string filepath);    // Reads the nodes - stores nodes at pointer
-    void save();                                                    // Saves the nodes to the filepath (reverse of reading the file)
-    void save(std::vector<Position4D>* n, std::string filepath);    // Saves the nodes at the input pointer to the inputted filepath
+    void read(std::string filepath);                                 // Reads the nodes - stores nodes + filepath locally
+    void read(std::vector<NodeDetails>* n, std::string filepath);    // Reads the nodes - stores nodes at pointer
+    void save();                                                     // Saves the nodes to the filepath (reverse of reading the file)
+    void save(std::vector<NodeDetails>* n, std::string filepath);    // Saves the nodes at the input pointer to the inputted filepath
 };
 
 #endif
