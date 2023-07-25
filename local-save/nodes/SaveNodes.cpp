aab69e8180dd20f11ae13dff49fa51d0251f239d
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-07-25 19:57:12 +1000

    Swapped Position4D for NodeDetails
    Swapped Position4D for NodeDetails in the SaveNodes framework implementation

--- local-save/nodes/SaveNodes.cpp
+++ local-save/nodes/SaveNodes.cpp
@@ -1,10 +1,6 @@
 #include "SaveNodes.h"
 
 
-/* x: Public Address
-    y: IP Address
-    z: Port
-    i: Bias */
 SaveNodes::SaveNodes(std::string filepath) {
     read(filepath);
 }
@@ -13,25 +9,25 @@ void SaveNodes::read(std::string filepath) {
     path = filepath;
     std::vector<std::string> data = reader.getData(&path);
 
-    Position4D temp;
+    NodeDetails temp;
     for(int i = 0; i < data.size() / 4; i++) {
-        temp.x = data[i * 3];                   // Node Public Address
-        temp.y = data[(i * 3) + 1];             // Node IP Address
-        temp.z = stoi(data[(i * 3) + 2]);       // Node Port
-        temp.i = stoull(data[(i * 3) + 3]);    // Node Bias
+        temp.address = data[i * 3];                // Node Public Address
+        temp.ip = data[(i * 3) + 1];               // Node IP Address
+        temp.port = stoi(data[(i * 3) + 2]);       // Node Port
+        temp.bias = stoull(data[(i * 3) + 3]);     // Node Bias
         nodes.push_back(temp);
     }
 }
 
-void SaveNodes::read(std::vector<Position4D>* n, std::string filepath) {
+void SaveNodes::read(std::vector<NodeDetails>* n, std::string filepath) {
     std::vector<std::string> data = reader.getData(&filepath);
 
-    Position4D temp;
+    NodeDetails temp;
     for(int i = 0; i < data.size() / 4; i++) {
-        temp.x = data[i * 3];                   // Node Public Address
-        temp.y = data[(i * 3) + 1];             // Node IP Address
-        temp.z = stoi(data[(i * 3) + 2]);       // Node Port
-        temp.i = stoull(data[(i * 3) + 3]);    // Node Bias
+        temp.address = data[i * 3];                // Node Public Address
+        temp.ip = data[(i * 3) + 1];               // Node IP Address
+        temp.port = stoi(data[(i * 3) + 2]);       // Node Port
+        temp.bias = stoull(data[(i * 3) + 3]);     // Node Bias
         n.push_back(temp);
     }
 }
@@ -40,23 +36,23 @@ void SaveNodes::save() {
     std::vector<std::string> data;
 
     for(int i = 0; i < nodes.size(); i++) {
-        data.push_back(nodes[i].x);
-        data.push_back(nodes[i].y);
-        data.push_back(std::to_string(nodes[i].z));
-        data.push_back(std::to_string(nodes[i].i));
+        data.push_back(nodes[i].address);
+        data.push_back(nodes[i].ip);
+        data.push_back(std::to_string(nodes[i].port));
+        data.push_back(std::to_string(nodes[i].bias));
     }
 
     writer.createFile(data, &path);
 }
 
-void SaveNodes::save(std::vector<Position4D>* n, std::string filepath) {
+void SaveNodes::save(std::vector<NodeDetails>* n, std::string filepath) {
     std::vector<std::string> data;
 
     for(int i = 0; i < n.size(); i++) {
-        data.push_back(n[i].x);
-        data.push_back(n[i].y);
-        data.push_back(std::to_string(n[i].z));
-        data.push_back(std::to_string(n[i].i));
+        data.push_back(n[i].address);
+        data.push_back(n[i].ip);
+        data.push_back(std::to_string(n[i].port));
+        data.push_back(std::to_string(n[i].bias));
     }
 
     writer.createFile(data, &filepath);
