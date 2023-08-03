6e9f596051195abbab66d1c7a39ceebf946ee88d
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-03 21:49:34 +1000

    Bandwidth now influences Bias!
    Made the constant refresh bias function take into account the node bandwidth - this adds the bandwidth onto the node bias (this will be a set predetermined amount in the future), and subtracts the previous bandwidth (previous Bandwidth is updated every time the function refreshes)

--- bias/Bias.h
+++ bias/Bias.h
@@ -10,11 +10,15 @@
 
 
 struct Bias {
+    std::vector<OtherNodes>* otherNodes;             // The nodes on the network
+    std::vector<unsigned short> previousBandwidth;   // The previous bandwidth - before bandwidth calculation was taken into account for node bias
+
     // Whenever the constructor is called, a thread is created and refreshBias is run constantly
-    Bias(Blockchain* blockchain, std::vector<OtherNodes>* nodes);
-    
-    void refreshBias(Blockchain* blockchain, std::vector<OtherNodes>* nodes);                                   // Refresh all node biases - done by going through the blockchain and looking for staked VLNC
-    void refreshBias(Blockchain* blockchain, std::string nodePublicAddress, std::vector<OtherNodes>* nodes);    // Refreshes a single node bias - done by going through the blockchain and looking for staked VLNC from the node public address
+    Bias(Blockchain* blockchain, std::vector<OtherNodes>* nodes, std::vector<unsigned short>* bandwidth);
+    ~Bias();
+
+    void refreshBias(Blockchain* blockchain, std::vector<OtherNodes>* nodes, std::vector<unsigned short>* bandwidth);                                   // Refresh all node biases - done by going through the blockchain and looking for staked VLNC
+    void refreshBias(Blockchain* blockchain, std::string nodePublicAddress, std::vector<OtherNodes>* nodes, std::vector<unsigned short>* bandwidth);    // Refreshes a single node bias - done by going through the blockchain and looking for staked VLNC from the node public address
     
     unsigned long getBias(std::string nodePublicAddress, std::vector<OtherNodes>* nodes);    // Gets the bias of a single node
     void setBandwidth(std::vector<std::vector<unsigned short>>* nodeBandwidth);               // Changes the bias according to the input (this is run in a second thread)
