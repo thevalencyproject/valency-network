9831e5e1e3d4ee701e147014fa751546d5bd5b72
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-09 21:25:40 +1000

    Added Bandwidth as a Node Parameter
    Added Bandwidth as a parameter to the node structure to allow the TransactionFeeSyncing framework to accurately determine network load locally

--- structures/Node.h
+++ structures/Node.h
@@ -10,16 +10,28 @@ struct NodeDetails {
     std::string address;        // The public address that is used to encrypt data sent to this node
     std::string ip;             // The networking IP address to connect to this node
     int port;                   // The networking port to connect to this node
+    unsigned long bandwidth;    // The bandwidth of this node
     unsigned long bias;         // The power the node has over the network
 };
 
-struct OtherNode {
-    NodeDetails details;
+struct NodeSync {
+    // Used to sync
+    std::vector<std::vector<Block>> unverifiedBlocks;           // The unverified blocks on the network (according to this node)
+    std::vector<std::pair<std::vector<Block>, unsigned short count>> unverifiedBlocks;
 
-    std::vector<std::vector<Block> unverifiedBlocks;            // The unverified blocks on the network (according to this node)
-    std::vector<NodeDetails> othersDetails;                     // The details of the other nodes on the network (according to this node)
+    std::vector<std::vector<NodeDetails>> othersDetails;        // The details of the nodes on the network (according to this node)
+
+    // Used to calculate node bias -> unsigned short = 2 bytes * numOfActiveNodes^2 = amount of space each of these vectors use (e.g. 1000 nodes = 2 megabytes)
     std::vector<std::vector<unsigned short>> othersStake;       // The stake of the other nodes on the network (according to this node)
     std::vector<std::vector<unsigned short>> othersBandwidth;   // The bandwidth of the other nodes on the network (according to this node)
+};
+
+struct Client {
+    NodeSync sync;
+
+    Blockchain blockchain;
+    Configuration configuration;
+    std::vector<NodeDetails> knownnodes;
 };
 
 #endif
