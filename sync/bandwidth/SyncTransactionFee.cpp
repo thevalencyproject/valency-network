0880c209137663813d5b110ebaa417890a00d84f
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-10 15:41:52 +1000

    Set up for more efficient communication system
    Set up for implementation of per-basis nodeaddress communication system

---
+++ sync/bandwidth/SyncTransactionFee.cpp
@@ -0,0 +1,73 @@
+#include "SyncTransactionFee.h"
+
+
+void SyncTransactionFee::syncExpiry() {
+    // Get the current expiry time (last 30s increment)
+    auto expirytimeDuration = std::chrono::system_clock::now().time_since_epoch() - (std::chrono::system_clock::now().time_since_epoch() % std::chrono::seconds(30));
+    
+    std::chrono::system_clock::time_point expirytime{expirytimeDuration};   // Convert to a time point
+    auto expirytime_t = std::chrono::system_clock::to_time_t(expirytime);   // Convert to time_t (allows stringstream - translate time into a string)
+
+    while(1) {
+        while(std::chrono::system_clock::now() - std::chrono::seconds(29) > expirytime) {   // Goes into loop @ 29s and then constantly loops to check for 30s (to reduce cpu overhead)
+            while(std::chrono::system_clock::now() - std::chrono::seconds(30) >= expirytime) {
+                expirytime = expirytime + std::chrono::seconds(30);                     // Add 30s to the chrono expirytime
+
+                expirytime_t = std::chrono::system_clock::to_time_t(expirytime);        // Convert to time_t
+                expiry.clear(); 
+                expiry << std::put_time(std::localtime(&expirytime_t), "%Y-%m-%d %X");  // Convert time_t to string stream
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));   // Sleep for 1s
+    }
+}
+
+void SyncTransactionFee::syncFee(std::vector<NodeDetails>* knownNodes) {
+    while(1) {
+        int numOfActiveNodes = 0;
+        for(int i = 0; i < knownNodes.size(); i++)
+            if(client.connectToServer(knownNodes.ip, 8082, NULL, '2') == true)   // Send a quit message
+                numOfActiveNodes++;    // Use the syncknownnodes framework port to determine if a node is active or not
+        
+        // Calculate the transaction fee's (THIS IS BASIC - NEEDS REVISION)
+        double basefee = (theoreticalMaxNetworkBandwidth - currentNetworkBandwidth + numOfActiveNodes) / 10000;    // The base fee per single transaction
+        for(int i = 1; i <= 10; i++)                                                                               // For each transaction after, multiply, and add 1:
+            transactionfees.push_back((basefee * i) + i);
+
+        // Get Theoretical Max
+        if(bandwidthUpdateTimer > std::chrono::system_clock::now() - std::chrono::minutes(10)) {    // If the time has increased by 10 minutes
+            for(int i = 0; i < &knownNodes.size(); i++) {                   // Update the max individual node bandwidths + theoreticalMaxNetworkBandwidth + current network bandwidth (combined network bandwidth)
+                // Theoretical Max Network Bandwidth
+                theoreticalMaxNetworkBandwidth = theoreticalMaxNetworkBandwidth - nodemaxbandwidths[i];      // Subtract the max for the current node
+
+                if(nodemaxbandwidths[i] > &knownNodes[i].bandwidth)         // Update if the max is higher than previous highs
+                    nodemaxbandwidths[i] = &knownNodes[i].bandwidth;
+
+                theoreticalMaxNetworkBandwidth = theoreticalMaxNetworkBandwidth + nodemaxbandwidths[i];      // Add the new/existing max for the current node
+
+                // Current Network Bandwidth
+                currentNetworkBandwidth = currentNetworkBandwidth - currentnodebandwidths[i];
+                currentNetworkBandwidth = currentNetworkBandwidth + &knownNodes[i].bandwidth;
+                currentnodebandwidths[i] = &knownNodes[i].bandwidth;
+            }
+            
+            theoreticalUpdateTimer = std::chrono::system_clock::now();   // Reset the timer
+        }
+    }
+}
+
+void SyncTransactionFee::sync(std::vector<NodeDetails>* knownNodes) {
+    std::thread expirysyncthread(syncExpiry());          // Create a thread that syncs the fee expiry time
+    std::thread feesyncthread(syncFee(knownNodes));      // Create a thread that syncs the fee amounts
+}
+
+std::pair<std::string, double> SyncTransactionFee::getTransactionFee(int numOfTransactions) {
+    if(numOfTransactions > 10 || numOfTransactions < 1)     // Invalid Number of Transactions
+        return;
+
+    std::pair<std::string, double> output;
+    output.first = expiry.str();
+    output.second = transactionfees[numOfTransactions];     // The first 10 values in transactionfees vector are non-onion routing
+    return output;
+}
---
+++ sync/bandwidth/SyncTransactionFee.h
@@ -0,0 +1,46 @@
+#ifndef SYNCTRANSACTIONFEE_H
+#define SYNCTRANSACTIONFEE_H
+
+#include <ctime>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <thread>
+#include <sstream>
+#include <iomanip>
+#include "structures/Node.h"
+#include "configuration/Configuration.h"
+#include "valency-core/custom-types/Position.h"
+#include "valency-core/networking/client/Client.h"
+
+
+/* Analyses Chain and Network Data to generate transaction fees
+        -> Run on network nodes and wallets (to determine validation rewards (node) + transaction fees (wallet))
+        -> Run in its own thread
+        -> Updates the transaction fee IF a new block is added AND the previous transaction fee has expired(timed out) */
+class SyncTransactionFee {
+private:
+    std::chrono::duration bandwidthUpdateTimer;         // Update the theoreticalMaxNetworkBandwidth every 10m
+    std::vector<unsigned int> nodemaxbandwidths;        // The max bandwidth of each node (of all time) - used to get the theoretical maximum network bandwidth
+    std::vector<unsigned int> currentnodebandwidths;    // The current bandwidths of each node
+    unsigned int theoreticalMaxNetworkBandwidth;        // [IN MB/s] The theoretical max bandwidth (max bandwidth for single node * numOfActiveNodes)
+    unsigned int currentNetworkBandwidth;               // [IN MB/s] The bandwidth of each active node added up
+
+    std::vector<double> transactionfees;   // Contains all transaction fee's for every combination of #ofTransactions & #ofOnionRoutingNodes
+    std::stringstream expiry;              // Contains the expiry time for the transaction fee's (do expiry.str() to get string version)
+
+    Client client;
+    Configuration config;
+
+    void syncExpiry();                                      // Syncs the expiry time - this increments by 30s every 30s - doesn't need to get current expiry from other nodes because it just increments by 30s every 30s (starting from 0s)
+    void syncFee(std::vector<NodeDetails>* knownNodes);     // Syncs the transaction fee - requires Knownnodes list to get the number of active nodes (used to determine network load and therefore fee's)
+
+public:
+    SyncTransactionFee() {};
+
+    void sync(std::vector<NodeDetails>* knownNodes);     // Creates and runs its own thread doing sync()   
+    
+    std::pair<std::string, double> getTransactionFee(int numOfTransactions);                         // Returns the transaction fee and expiry
+};
+
+#endif
