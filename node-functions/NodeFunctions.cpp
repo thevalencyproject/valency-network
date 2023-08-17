53313a7f98ba0d1cd857e7110596183871bec216
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-17 21:57:06 +1000

    Added copyright disclaimer to functions frameworks

--- node-functions/NodeFunctions.cpp
+++ node-functions/NodeFunctions.cpp
@@ -1,3 +1,6 @@
+/* This file is a part of The Valency Project which is released under the GNU Lesser General Public License v2.1
+   Go to https://github.com/thevalencyproject/valency-core/blob/main/LICENSE for further license details.        */
+
 #include "NodeFunctions.h"
 
 
