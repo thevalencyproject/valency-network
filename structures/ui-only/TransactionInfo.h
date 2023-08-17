412fff7d8ecb76fdc6afae253b1459599caf778c
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-17 21:57:57 +1000

    Added Copyright Disclaimer to Structures

--- structures/ui-only/TransactionInfo.h
+++ structures/ui-only/TransactionInfo.h
@@ -1,3 +1,6 @@
+/* This file is a part of The Valency Project which is released under the GNU Lesser General Public License v2.1
+   Go to https://github.com/thevalencyproject/valency-core/blob/main/LICENSE for further license details.        */
+
 #ifndef TRANSACTIONINFO_H
 #define TRANSACTIONINFO_H
 
