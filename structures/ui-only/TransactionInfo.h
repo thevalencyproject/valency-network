ec927751617fc0ce27c67e827abb8c29c4d708a5
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-17 22:04:50 +1000

    Fixed Wrong License Link
    Fixed the license link in all the disclaimers linking to the valency-core repository... the link should now send users to the valency-network repository license file.

--- structures/ui-only/TransactionInfo.h
+++ structures/ui-only/TransactionInfo.h
@@ -1,5 +1,5 @@
 /* This file is a part of The Valency Project which is released under the GNU Lesser General Public License v2.1
-   Go to https://github.com/thevalencyproject/valency-core/blob/main/LICENSE for further license details.        */
+   Go to https://github.com/thevalencyproject/valency-network/blob/main/LICENSE for further license details.        */
 
 #ifndef TRANSACTIONINFO_H
 #define TRANSACTIONINFO_H
--- sync/Sync.h
+++ sync/Sync.h
@@ -1,5 +1,5 @@
 /* This file is a part of The Valency Project which is released under the GNU Lesser General Public License v2.1
-   Go to https://github.com/thevalencyproject/valency-core/blob/main/LICENSE for further license details.        */
+   Go to https://github.com/thevalencyproject/valency-network/blob/main/LICENSE for further license details.        */
 
 #ifndef SYNC_H
 #define SYNC_H
